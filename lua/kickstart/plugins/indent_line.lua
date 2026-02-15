04f2adc4612cad853e4288284f36e44b3d0e72ed
Francis <info@francisqureshi.com>
2026-02-15 21:39:36 +0000

    snacks

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,17 +1,23 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'saghen/blink.indent',
-    -- See: https://github.com/saghen/blink.indent
-    --- @module 'blink.indent'
-    --- @type blink.indent.Config
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
     opts = {
-      static = {
-        enabled = true, -- Enable static indent guides
-        char = '▏', -- Thinner character (alternatives: '│', '┊', '┆', '¦', '|', '⁞')
-      },
-      scope = {
-        enabled = false, -- Disable scope highlighting
-      },
+      indent = { enabled = true },
     },
+    config = function(_, opts)
+      require('snacks').setup(opts)
+      -- Fix invisible indent guides with kanso colorscheme
+      local function set_indent_hl()
+        vim.api.nvim_set_hl(0, 'SnacksIndent', { fg = '#2a2f36' })
+        vim.api.nvim_set_hl(0, 'SnacksIndentScope', { fg = '#9a8aab' }) -- muted purple
+      end
+      set_indent_hl()
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        callback = set_indent_hl,
+      })
+    end,
   },
 }
